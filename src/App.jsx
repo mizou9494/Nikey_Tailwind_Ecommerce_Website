dae39bf0247cdeaa0454f70e76a574d39177d012
hamza bitar <hamza.7.bitar@gmail.com>
2023-10-26 00:53:00 +0100

    getting through with the design...

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,12 @@ import Services from './sections/Services'
 import SpecialOffer from './sections/SpecialOffer'
 import Subscribe from './sections/Subscribe'
 import SuperQuality from './sections/SuperQuality'
+import Nav from './components/Nav'
 
 export default function App() {
   return (
     <main className="relative">
-    {/* Nav */}
+    <Nav/>
     <section className="xl:padding-1 wide:padding-r padding-b">
       <Hero/>
     </section>
