6585c1f9007dacb331ff71bfca865b64145d073c
hamza bitar <hamza.7.bitar@gmail.com>
2023-10-26 01:32:41 +0100

    still styling the Hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -9,19 +9,19 @@ const Hero = () => {
       className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
     >
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
-        <p>Our summer collection</p>
-        <h1>
-          <span>The New Arrival</span>
+        <p className='text-xl font-montserrat text-coral-red'>Our summer collection</p>
+        <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold'>
+          <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>The New Arrival</span>
           <br />
-          <span>Nike</span>
+          <span className='text-coral-red inline-block mt-3 mr-2'>Nike </span>
           Shoes
         </h1>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur dolore ipsa iusto.</p>
+        <p className='font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur dolore ipsa iusto.</p>
         <Button label="Shop now" iconURL={arrowRight}/>
 
         <div className="flex justify-starts items-start flex-wrap w-full mt-20 gap-16">
-          {statistics.map((stat, index) => (
-            <div>
+          {statistics.map( stat => (
+            <div key={stat.label}>
               <p> { stat.value } </p>
               <p> { stat.label } </p>
             </div>
