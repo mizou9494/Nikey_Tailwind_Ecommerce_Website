747651e52e0b9b5b97f09fafa5a41d1bb4ea0878
hamza bitar <hamza.7.bitar@gmail.com>
2023-10-26 21:46:59 +0100

    first section is almost done with the image selection and all

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,12 @@
 import { arrowRight } from '../assets/icons'
+import { bigShoe1 } from '../assets/images'
 import Button from '../components/Button'
-import { statistics } from '../constants'
+import { shoes, statistics } from '../constants'
+import ShoeCard from '../components/ShoeCard'
+import { useState } from 'react'
 
 const Hero = () => {
+  const [bigShoeImg, setBigShoeImg] = useState(bigShoe1)
   return (
     <section
       id="home"
@@ -22,10 +26,32 @@ const Hero = () => {
         <div className="flex justify-starts items-start flex-wrap w-full mt-20 gap-16">
           {statistics.map( stat => (
             <div key={stat.label}>
-              <p> { stat.value } </p>
-              <p> { stat.label } </p>
+              <p className="text-4xl font-palanquin font-bold"> { stat.value } </p>
+              <p className='leading-7 font-mono text-slate-gray'> { stat.label } </p>
             </div>
           ))}
+        </div>
+      </div>
+
+      <div className='relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-right'>
+        <img 
+          src={bigShoeImg} 
+          alt="shoe collection" 
+          width={610} 
+          height={500}
+          className='object-contain relative z-10' 
+        />
+
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6 ">
+            {shoes.map(shoe => (
+              <div key={shoe}>
+                <ShoeCard 
+                  imgURL={shoe}
+                  changeBigShoeImage={(shoe) => {setBigShoeImg(shoe)}}
+                  bigShoeImage={bigShoeImg}
+                />
+              </div>
+            ))}
         </div>
       </div>
     </section>
